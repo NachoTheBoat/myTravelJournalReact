0d2cf439ddbf542994e5170773ae0208a1878ada
Ignacio Iglesias <nacho34k@gmail.com>
2023-03-16 15:22:47 -0300

    test

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export default [
     title: "Humahuaca",
     description:
       "Humahuaca es la ciudad cabecera del departamento homónimo, en la provincia de Jujuy, Argentina. Está ubicada a lo largo de la RN9 al norte de San Salvador de Jujuy, a 2.939 m s. n. m.; está organizada con una Municipalidad y diferentes centros vecinales; hasta finales del s. XIX fue uno de los más importantes centros comerciales coloniales del antiguo camino al Alto Perú. En ella se destacan sus calles angostas y empedradas, dignas de recorrer a pie, con sus casas bajas de adobe conservando su fisonomía histórica",
-    image: "/public/humahuaca.jpg",
+    image: "./public/images/humahuaca.jpg",
     pais: "Argentina",
     location: "https://goo.gl/maps/uwkRPAL9fByeY3Z4A",
   },
@@ -13,7 +13,7 @@ export default [
     title: "Viena",
     description:
       "Ubicado en el centro de Europa. Viena (en alemán: Wien, pronunciado /viːn/ es una ciudad austriaca situada a orillas del Danubio, en el valle de los Bosques de Viena, al pie de las primeras estribaciones de los Alpes. Es la capital federal y ciudad principal del país, además de uno de sus nueve estados federados (Bundesland Wien).",
-    image: "./public/viena.jpg",
+    image: "./public/images/viena.jpg",
     pais: "Austria",
     location: "https://goo.gl/maps/L4P7jgse3TCFeeXF7",
   },
@@ -22,7 +22,7 @@ export default [
     title: "Porto Alegre",
     description:
       "Porto Alegre es la capital y ciudad más grande del estado brasileño de Río Grande del Sur. Cuenta con una población de 1.420.667 habitantes y de 4.276.475 de habitantes para su área metropolitana. La ciudad está ubicada en la conjunción de cinco ríos, entre los cuales se encuentra el río Guaíba (que en realidad es un lago), continuación del río Jacuí poco antes de su desembocadura en la laguna de los Patos. Porto Alegre es un gran centro industrial del sur de Brasil y está ubicado en una zona estratégica para el Mercosur, debido a su proximidad con respecto a Buenos Aires, Montevideo, Asunción, Rosario y Córdoba. Los habitantes de este estado son llamados gaúchos, el plato típico es el churrasco (carne asada) y el mate es la infusión tradicional.",
-    image: "../public/poa.jpg",
+    image: "./public/images/poa.jpg",
     pais: "Brasil",
     location: "https://goo.gl/maps/6sTLArL6D7Hy8xM76",
   },
@@ -31,7 +31,7 @@ export default [
     title: "Buenos Aires",
     description:
       "Capital del Actual campeón del mundo. Buenos Aires es la capital y ciudad más poblada de la República Argentina. Sus nombres oficiales son Ciudad de Buenos Aires o Ciudad Autónoma de Buenos Aires (CABA).10​ También es llamada Capital Federal, por ser la sede del gobierno nacional.11​ Es uno de los 24 distritos, o «jurisdicciones de primer orden»12​ que conforman el país. Desde 1996 es una ciudad autónoma; tiene sus propios poderes ejecutivo, legislativo y judicial. Está situada en la región centro-este del país, sobre la orilla sur del Río de la Plata, en la región pampeana. La Ciudad de Buenos Aires fue cedida en 1880 por la Provincia de Buenos Aires para que fuera la capital federal del país; a partir de 2020 es considerada «capital principal», junto a 24 capitales alternas.13​ En virtud de la reforma constitucional de 1994 goza de un régimen de autonomía.",
-    image: "./public/buenosaires.jpg",
+    image: "./public/images/buenosaires.jpg",
     pais: "Argentina",
     location: "https://goo.gl/maps/FR72sm6skvbKbPzP6",
   },
@@ -40,7 +40,7 @@ export default [
     title: "Montevideo",
     description:
       "Montevideo es la capital de la República Oriental del Uruguay y del departamento homónimo. Se ubica en la zona sur del país, contando con costas sobre el Río de la Plata, donde se ubican la bahía de Montevideo y el principal puerto de la región.5​6​ El censo de 2011 registró 1 319 108 habitantes en el departamento y 1 947 604 en el área metropolitana,7​ lo que la ubica en el trigésimo puesto en América del Sur.",
-    image: "./public/montevideo.jpeg",
+    image: "./public/images/montevideo.jpeg",
     pais: "Uruguay",
     location: "https://goo.gl/maps/3YuoQdNLgQ6MirKDA",
   },
@@ -49,7 +49,7 @@ export default [
     title: "Trieste",
     description:
       "Ubicada a la orilla del mal adriatico, es una ciudad situada en el norte de Italia, a orillas del mar Adriático; hace frontera con Eslovenia. Cuenta con 205 593 habitantes (2010) y disfruta de un clima suave y soleado, excepto cuando sopla la bora, un viento que alguna vez al año alcanza los 100 km/h. Es la capital de la provincia del mismo nombre y de la región Friul-Venecia Julia.",
-    image: "./public/trieste.jpg",
+    image: "./public/images/trieste.jpg",
     pais: "Italia",
     location: "https://goo.gl/maps/J7amUDPwMULi6TwR9 ",
   },
